93c95f191f10916ba385d1a9bf2ac3a3025f54ef
jose13001 <josecarlos_01_30@unitec.edu>
2022-02-11 17:36:52 -0600

    trabajando en la clase main

--- src/lab4p2_josereyes_21941105/Lab4P2_JoseReyes_21941105.java
+++ src/lab4p2_josereyes_21941105/Lab4P2_JoseReyes_21941105.java
@@ -17,7 +17,7 @@ public class Lab4P2_JoseReyes_21941105 {
     static Scanner lea =new Scanner(System.in);
     
     public static void main(String[] args) {
-        
+        agFamilias();
         
         int opc;
         
@@ -43,9 +43,14 @@ public class Lab4P2_JoseReyes_21941105 {
             
             case 2:
                 System.out.println("****Crear Aldeano****");
+                CrearAldeano();
             break;
             
             case 3:
+                System.out.println("****Imprimir Familias****");
+                for(Familias fam : familia){
+                    System.out.println(fam.toString());
+                }
             break;
             
             case 4: 
@@ -57,17 +62,75 @@ public class Lab4P2_JoseReyes_21941105 {
         }while(opc!=0);
         
     }
+    public static void agFamilias(){
+        familia.add(new Familias("Montesco"));
+        familia.get(0).aldeanos.add(new SuperGranjero("Romeo","Montesco",18,1000));
+        familia.get(0).aldeanos.add(new Herrero("marcelo","Montesco",25,350));
+        familia.get(0).aldeanos.add(new Agronomo("Alex","Montesco",22,10));
+        familia.add(new Familias("Capuleto"));
+        familia.get(1).aldeanos.add(new Normales("Julieta","Capuleto",17,1000));
+        familia.get(1).aldeanos.add(new Pacifistas("Pedro","Capuleto",40,150));
+        familia.get(1).aldeanos.add(new Herrero("Leonardo","Capuleto",17,300));
+        familia.add(new Familias("Reyes"));
+        familia.get(2).aldeanos.add(new Herrero("Jose","Reyes",20,600));
+        familia.get(2).aldeanos.add(new Normales("Julio","Reyes",13,400));
+        familia.get(2).aldeanos.add(new Pacifistas("Juana","Reyes",50,250));
+    }
+    
+    
     public static boolean CrearFamilia(){
         System.out.print("Ingrese apellido: ");
-        String apellido = lea.nextLine();
+        String apellido = lea.next();
         for (Familias fam :familia){
             if (fam.apellido.equals(apellido)){
                 System.out.println("Esta familia ya existe");
                 return false;
             }
         }
+        System.out.println("****************");
         familia.add(new Familias(apellido));
         return true;
     }
+    public static boolean CrearAldeano(){
+        System.out.print("Ingrese nombre: ");
+        String nombre=lea.next();
+        System.out.print("Ingrese apellido: ");
+        String apellido = lea.next();
+        System.out.print("Ingrese edad: ");
+        int edad = lea.nextInt();
+        System.out.print("Ingrese puntos de vida: ");
+        int puntosVida = lea.nextInt();
+        for (Familias fam :familia){
+            if (fam.apellido.equals(apellido)){
+                System.out.println("1 - Normal\n2 - Pacifista\n3 - Herrero\n4 - Agrónomo\n5 - Explosivo");
+                System.out.print("Ingrese el tipo de aldeano: ");
+                int tipo = lea.nextInt();
+                
+                switch(tipo){
+                    case 1:
+                        fam.aldeanos.add(new Normales (nombre, apellido, edad, puntosVida));
+                        break;
+                    case 2:
+                        fam.aldeanos.add(new Pacifistas (nombre, apellido, edad, puntosVida));
+                        break;
+                    case 3:
+                        fam.aldeanos.add(new Herrero (nombre, apellido, edad, puntosVida));
+                        break;
+                    case 4:
+                        fam.aldeanos.add(new Agronomo (nombre, apellido, edad, puntosVida));
+                        break;
+                    case 5:
+                        fam.aldeanos.add(new Explosivo (nombre, apellido, edad, puntosVida));
+                        break;
+                }
+                System.out.println("***************");
+                return true;
+            }
+        }
+        System.out.println("La familia "+ apellido+" no existe");
+       return false; 
+    }
+    
+    
     
 }

ef4017e22ec37d34065447cb37389275a85d25af
jose13001 <josecarlos_01_30@unitec.edu>
2022-02-11 13:52:59 -0600

    se empieza a rezar

---
+++ src/lab4p2_josereyes_21941105/Lab4P2_JoseReyes_21941105.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab4p2_josereyes_21941105;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author josec
+ */
+public class Lab4P2_JoseReyes_21941105 {
+
+    
+    public static void main(String[] args) {
+        Scanner lea =new Scanner(System.in);
+        System.out.println("******MENU******");
+        System.out.println("0. Salir");
+        System.out.println("1. Crear Familia");
+        System.out.println("2. Crear Aldeano");
+        System.out.println("3. Imprimir Familias");
+        System.out.println("4. Pelear");
+        System.out.println("Ingrese una opcion: ");
+        int opc=lea.nextInt();
+        switch(opc){
+            case 0:{
+                break;
+            }
+            case 1:{
+                
+            }
+            break;
+            case 2:{
+                
+            }
+            break;
+            case 3:{
+                
+            }
+            break;
+            case 4:{
+                
+            }
+            break;
+        }
+        
+        
+       
+        
+        
+    }
+    
+}
